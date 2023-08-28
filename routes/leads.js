0aeae4fd74e5bf9a82ed229b78debe12a7d32fb4
KalaivaniBaskar <kalaivani.working@gmail.com>
2023-08-28 15:22:17 +0530

    update

--- routes/leads.js
+++ routes/leads.js
@@ -5,10 +5,10 @@ import { verifyAccessToken } from '../middleware/verifyAccessToken.js';
 
 const router = express.Router();
 
-router.post('/get-leads', verifyAccessToken, verifyRolePermission(USER_ROLES.Marketing, USER_ROLES.Admin), getLeads )
-router.post('/get-customers', verifyAccessToken, verifyRolePermission(USER_ROLES.Marketing, USER_ROLES.Admin, USER_ROLES.Sales), getCustomers )
-router.post('/get-product', verifyAccessToken, verifyRolePermission(USER_ROLES.Marketing, USER_ROLES.Admin, USER_ROLES.Sales), getCustomers )
-router.post('/add-leads', verifyAccessToken, verifyRolePermission(USER_ROLES.Marketing, USER_ROLES.Admin), addNewLead )
-router.post('/update-leads', verifyAccessToken, verifyRolePermission(USER_ROLES.Marketing, USER_ROLES.Admin), getLeads )
+// router.post('/get-leads', verifyAccessToken, verifyRolePermission(USER_ROLES.Marketing, USER_ROLES.Admin), getLeads )
+// router.post('/get-customers', verifyAccessToken, verifyRolePermission(USER_ROLES.Marketing, USER_ROLES.Admin, USER_ROLES.Sales), getCustomers )
+// router.post('/get-product', verifyAccessToken, verifyRolePermission(USER_ROLES.Marketing, USER_ROLES.Admin, USER_ROLES.Sales), getCustomers )
+// router.post('/add-leads', verifyAccessToken, verifyRolePermission(USER_ROLES.Marketing, USER_ROLES.Admin), addNewLead )
+// router.post('/update-leads', verifyAccessToken, verifyRolePermission(USER_ROLES.Marketing, USER_ROLES.Admin), getLeads )
 
 export const leadsRouter = router;
