2722899d4d53c75b676ca7dbabaa7ed8511c92da
KalaivaniBaskar <kalaivani.working@gmail.com>
2023-08-29 16:50:27 +0530

    update

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -333,9 +333,6 @@ export const getProductSold = async(req,res) => {
             order_status : 1,
            order_items : 1, 
            order_date : 1,
-           products : {
-             $substr : ["$order_items.product_ID" , 0,2]
-           },
            year : {
             $substr : [ "$order_date", 0,4]
           }   
